fdec7e5104ce4cfd68237555fc9250237bfeaa52
github username <github email address>
2018-03-10 01:02:52 -0500

    Social Sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
   resources :users, only: [:show, :edit, :update]
   root "movies#index"
   resources :movies do
